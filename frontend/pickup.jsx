85257dd8aa71b356db089b2c01f0644faa67163f
Jonathan Bae <jonbae213@gmail.com>
2019-12-09 17:42:09 -0500

    Fixed file tree for controllers

--- frontend/pickup.jsx
+++ frontend/pickup.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-import { signup, logout, login } from './util/session_util';
+// import { signup, logout, login } from './util/session_util';
 
 document.addEventListener("DOMContentLoaded", () => {
  
   const root = document.getElementById("root");
   const store = configureStore();
-  window.getState = store.getState();
-  window.dispatch = store.dispatch();
-  window.signup = signup;
-  window.login = login;
-  window.logout = logout;
+  // window.getState = store.getState();
+  // window.dispatch = store.dispatch();
+  // window.signup = signup;
+  // window.login = login;
+  // window.logout = logout;
   ReactDOM.render(<div>this</div>, root)
 });
